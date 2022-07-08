d1a35d17fd47fd5f9995d742cbf4f8a059693cc9
kuzntspo <108883547+kuzntspo@users.noreply.github.com>
2022-07-08 20:25:02 +0300

    Осуществление перехода на новые окна

--- src/main/java/com/example/clientserverapp/Controller.java
+++ src/main/java/com/example/clientserverapp/Controller.java
@@ -1,11 +1,16 @@
 package com.example.clientserverapp;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class Controller {
 
@@ -29,8 +34,42 @@ public class Controller {
 
     @FXML
     void initialize() {
+        SignUpButton.setOnAction(actionEvent -> {
+            SignUpButton.getScene().getWindow().hide();
+
+            FXMLLoader loader=new FXMLLoader();
+            loader.setLocation(getClass().getResource("signUpView.fxml"));
+
+            try {
+                loader.load();
+            }
+            catch (IOException e){
+                e.printStackTrace();
+            }
+
+            Parent root = loader.getRoot();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.showAndWait();
+        });
+
         SignInButton.setOnAction(actionEvent -> {
-            System.out.println("2");
+            SignInButton.getScene().getWindow().hide();
+
+            FXMLLoader loader=new FXMLLoader();
+            loader.setLocation(getClass().getResource("secondView.fxml"));
+
+            try {
+                loader.load();
+            }
+            catch (IOException e){
+                e.printStackTrace();
+            }
+
+            Parent root = loader.getRoot();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.showAndWait();
         });
     }
 }
